a73c16e287cc59543ed48f5edb9cbd3a19691f41
DM <dearahmbarsolasco@gmail.com>
2025-08-19 17:06:19 +0800

    fixed frontend

--- frontend/src/pages/interns.tsx
+++ frontend/src/pages/interns.tsx
@@ -1,3 +1,4 @@
+//interns.tsx
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import {
@@ -545,7 +546,7 @@ const Interns: React.FC = () => {
                       <div className="flex items-center gap-1 mt-1">
                         <Clock className="w-5 h-5 text-gray-600" /> 
                         <span>
-                          {intern.total_hours - intern.time_remain}/{intern.total_hours} hours
+                          {(intern.total_hours - intern.time_remain).toFixed(2)}/{intern.total_hours} hours
                         </span>
                       </div>
                     </div>
